cf1756ebc505658ea94292f79f691aab1ac1aedc
Erick_Carrasco <erickfabianct@gmail.com>
2023-07-14 18:40:54 -0500

    Actualizacion de cambios - levantamiento de observacion

--- src/components/CardStatistics/CardStatistics.jsx
+++ src/components/CardStatistics/CardStatistics.jsx
@@ -1,62 +1,30 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './CardStatistics.scss'
 
-const CardStatistics =(props) =>{   
+const CardStatistics = ({ num, symbol, icon, text }) => {
+  const [count, setCount] = useState(0)
 
-    const{num, simbol, icono,text}=props
-    
-    const[count,setCount]=useState(0)
+  useEffect(() => {
+    let interval
+    if (count < num) {
+      const increment = (num === 700 ? 20 : 1)
+      interval = setInterval(() => setCount((prevCount) => prevCount + increment), num === 20 ? 40 : num === 99 ? 5 : 20)
+    }
+    return () => clearInterval(interval)
+  }, [count, num])
 
-
-    useEffect(() => {  
-        
-        if(count<num){
-            let interval
-            if(num===700){
-                interval = setInterval(() => {
-                    setCount(count+20) 
-                }, 20);
-            }
-            else if(num===99){
-                interval = setInterval(() => {
-                    setCount(count+1) 
-                }, 5);
-            }
-            else if(num===50){
-                interval = setInterval(() => {
-                    setCount(count+1) 
-                }, 20);
-            }
-            else{
-                interval = setInterval(() => {
-                    setCount(count+1) 
-                },40);
-            }                   
-                
-            return () => {
-                clearInterval(interval);
-          };
-
-        }
-                
-      }, [count]);
-
-    return(
-    <div className='container__card'>
-        <i className='container__card--icono'>{icono}</i>
-        <div className='container__card_content'>
+  return (
+    <div className='statistics-card'>
+        <i className='statistics-card--icono'>{icon}</i>
+        <div className='statistics-card_content'>
             <h3>
-               <span className='container__card_content--count'>{count}</span>
-                <span className='container__card_content--simbolo'>{simbol}</span>                
+               <span className='statistics-card_content--count'>{count}</span>
+                <span className='statistics-card_content--simbolo'>{symbol}</span>
             </h3>
-            <p className='container__card_content--text'>{text}</p>
+            <p className='statistics-card_content--text'>{text}</p>
         </div>
     </div>
-    
-    )
- 
+  )
 }
-
-
 
 export default CardStatistics
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,13 +3,11 @@ import Main from '../components/Main/Main'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
 import './App.scss'
-import Statistics from '../components/Statistics/Statistics'
 
 function App () {
   return (
     <>
       <Header />
-      <Statistics></Statistics>
       <Main>
         {/** Acá van los componentes de tipo Page */}
       </Main>
