4db0a9f9a89819469b4bc60b82d8137be679b710
Juan Villegas <juanvj0425@gmail.com>
2023-07-19 18:24:13 -0500

    Solventando conflictos HomePage

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -4,13 +4,15 @@ import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import "./App.scss";
 import AppointmentForm from "../components/MS-3 Form/AppointmentForm";
+import WhoWeAre from "../components/MS-4 WhoWeAre/WhoWeAre";
 
 function App() {
   return (
     <>
-      <Header />    
+      <Header />
       <Main>
-         <AppointmentForm />
+        <AppointmentForm />
+        <WhoWeAre />
       </Main>
       <Footer />
     </>
