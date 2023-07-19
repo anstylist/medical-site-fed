df055de3c7403b279254c7e92fb1debb812c4be7
Andry Peña <116394447+andrystylist@users.noreply.github.com>
2023-07-19 17:16:09 -0500

    Feature/ms 2 header (#9)
    * feat: Added the main layout for the App Header, MS-2
    
    * feat: Added responsive support, MS-2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,40 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.scss'
-import logo from '../../assets/logo.svg'
-import { HiMenu } from 'react-icons/hi'
+import HeaderContact from './HeaderContact'
+import MainMenu from './MainMenu'
+import HeaderRight from './HeaderRight'
+
+const urlLogo = 'https://res.cloudinary.com/dzmkilinu/image/upload/v1689393390/medical-site/logo_bcp977.png'
+const RIGHT_MENU = 'RIGHT_MENU'
+const MAIN_MENU = 'MAIN_MENU'
 
 function Header () {
+  const [openMenu, setOpenMenu] = useState(null)
+
   return (
-    <header className='header'>
-      <section className='header__container'>
-        <section className='header__left'>
-          <img className="header__logo"
-            src={logo}
-            alt="logo"
-            aria-label='Logo'
+    <>
+      <HeaderContact />
+      <header className='header'>
+        <section className='header__container'>
+          <section className='header__item header-section'>
+            <img className="header-section__logo"
+              src={urlLogo}
+              alt="logo"
+              aria-label='Logo Medical Site'
+            />
+          </section>
+          <MainMenu
+            onMenuOpen={() => setOpenMenu(MAIN_MENU)}
+            mustHideMenu={openMenu !== MAIN_MENU}
           />
-          <h1 className='header__title'>Medical Site</h1>
+          <HeaderRight
+            onMenuOpen={() => setOpenMenu(RIGHT_MENU)}
+            mustHideMenu={openMenu !== RIGHT_MENU}
+          />
         </section>
-        <section className='header__right'>
-          <nav className='header__nav'>
-            <ul className='header__menu'>
-              <li className='header__menu--item'>
-                <a href='#'>Medicines</a>
-              </li>
-              <li className='header__menu--item'>
-                <a href='#'>Doctors</a>
-              </li>
-              <li className='header__menu--item'>
-                Get started
-              </li>
-            </ul>
-            <button type='button'>
-              <HiMenu className='header__menu--icon' />
-            </button>
-          </nav>
-        </section>
-      </section>
-    </header>
+      </header>
+    </>
   )
 }
 
