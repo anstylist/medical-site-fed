267993edce8a45fa5275ecfa3028caefe6282c49
Juan Villegas <juanvj0425@gmail.com>
2023-07-19 18:41:19 -0500

    Solving conflicts for second-1 commit

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,36 +1,36 @@
-import CardStatistics from "../CardStatistics/CardStatistics"
-import { SlBadge } from "react-icons/sl";
-import { BiLike } from "react-icons/bi";
-import { FaBed } from "react-icons/fa";
-import { FaUserDoctor} from "react-icons/fa6";
+import React from 'react'
+import CardStatistics from '../CardStatistics/CardStatistics'
+import { SlBadge } from 'react-icons/sl'
+import { BiLike } from 'react-icons/bi'
+import { FaBed } from 'react-icons/fa'
+import { FaUserDoctor } from 'react-icons/fa6'
 import './Statistics.scss'
 
-const Statistics =()=>{
+const Statistics = () => {
+  const infoStatistics = [
+    { num: 20, symbol: '+', icon: <SlBadge size={34} />, text: 'Years of experience' },
+    { num: 99, symbol: '%', icon: <BiLike size={34} />, text: 'Patients satisfied' },
+    { num: 700, symbol: '+', icon: <FaBed size={34} />, text: 'Medical beds' },
+    { num: 50, symbol: '+', icon: <FaUserDoctor size={34} />, text: 'Laboratory Experts' }
+  ]
 
-    const infoStatistics =[
-        {num:20, simbol:'+', icono: <SlBadge size={34}/>,text:'Years of experience'},
-        {num:99, simbol:'%', icono: <BiLike size={34}/>,text:'Patients satisfied'},
-        {num:700, simbol:'+', icono: <FaBed size={34}/>,text:'Medical beds'},
-        {num:50, simbol:'+', icono: <FaUserDoctor size={34}/>,text:'Laboratory Experts'}
-    ]
-
-    return (
-        <div className="container_statistics">        
-            <div className="container_cardstatistics">
-            {infoStatistics.map((item,index)=>{
-                return (
+  return (
+    <div className="statistics">
+        <div className="list-statiscs-card">
+        {infoStatistics.map((item, index) => {
+          return (
                 <CardStatistics
-                    key={index}
-                    num={item.num} 
-                    simbol={item.simbol} 
-                    icono={item.icono} 
-                    text={item.text} 
+                key={index}
+                num={item.num}
+                symbol={item.symbol}
+                icon={item.icon}
+                text={item.text}
                 />)
-                })}
-            </div>
+        })
+        }
         </div>
-  
-    )
+    </div>
+  )
 }
 
 export default Statistics
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import Main from "../components/Main/Main";
 import Footer from "../components/Footer/Footer";
@@ -5,17 +6,27 @@ import Header from "../components/Header/Header";
 import "./App.scss";
 import AppointmentForm from "../components/MS-3 Form/AppointmentForm";
 import WhoWeAre from "../components/MS-4 WhoWeAre/WhoWeAre";
+=======
+import React from 'react'
+import Main from '../components/Main/Main'
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
+import './App.scss'
+>>>>>>> cf1756e (Actualizacion de cambios - levantamiento de observacion)
 
 function App() {
   return (
     <>
       <Header />
 <<<<<<< HEAD
+<<<<<<< HEAD
       <Main>
         <AppointmentForm />
         <WhoWeAre />
 =======
       <Statistics></Statistics>
+=======
+>>>>>>> cf1756e (Actualizacion de cambios - levantamiento de observacion)
       <Main>
         {/** Acá van los componentes de tipo Page */}
 >>>>>>> 1006d1e (MS-5 - Create component Statistics)
