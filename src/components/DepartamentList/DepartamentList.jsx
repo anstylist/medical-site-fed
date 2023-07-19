8ed8b5b3f2996a7327d88453f68fc5b323337ea2
Juan Villegas <juanvj0425@gmail.com>
2023-07-19 18:44:18 -0500

    Solving conflicts for second-2 commit

---
+++ src/components/DepartamentList/DepartamentList.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './DepartamentList.scss'
+import DepartamentCard from '../DepartamentCard/DepartamentCard'
+import { GiHeartOrgan, GiTemplarEye, GiStomach /** GiBrokenBone, GiBrain, GiAbdominalArmor */ } from 'react-icons/gi'
+
+const DepartamentList = () => {
+  const arrayDepartament = [
+    { id: 1, icon: <GiStomach size={34}/>, title: 'Gastroenterologist', information: 'Curabitur aliquet quam id dui posuere blandit. lacinia eget consectetur sed.' },
+    { id: 2, icon: <GiHeartOrgan size={34}/>, title: 'Cardiology', information: 'Curabitur aliquet quam id dui posuere blandit. lacinia eget consectetur sed.' },
+    { id: 3, icon: <GiTemplarEye size={34}/>, title: 'Ophthalmology', information: 'Curabitur aliquet quam id dui posuere blandit. lacinia eget consectetur sed.' }
+    /**
+     * - Solo lo use para una prueba, ya que este componente se va reutilizar mas adelante -
+    { id: 4, icon: <GiBrokenBone size={34}/>, title: 'Orthopedics', information: 'Curabitur aliquet quam id dui posuere blandit. lacinia eget consectetur sed.' },
+    { id: 5, icon: <GiBrain size={34}/>, title: 'Neurology', information: 'Curabitur aliquet quam id dui posuere blandit. lacinia eget consectetur sed.' },
+    { id: 6, icon: <GiAbdominalArmor size={34}/>, title: 'Plastic surgeons', information: 'Curabitur aliquet quam id dui posuere blandit. lacinia eget consectetur sed.' }
+  */
+  ]
+  return (
+    <div className='list-departament'>
+      {arrayDepartament.map((item) => {
+        return (
+          <DepartamentCard
+          key={item.id}
+          id={item.id}
+          icon={item.icon}
+          title={item.title}
+          information={item.information}
+        />)
+      })
+      }
+    </div>
+  )
+}
+export default DepartamentList
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -11,6 +11,8 @@ import React from 'react'
 import Main from '../components/Main/Main'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
+import OurDepartament from '../components/OurDepartament/OurDepartament'
+import Statistics from '../components/Statistics/Statistics'
 import './App.scss'
 >>>>>>> cf1756e (Actualizacion de cambios - levantamiento de observacion)
 
@@ -29,7 +31,12 @@ function App() {
 >>>>>>> cf1756e (Actualizacion de cambios - levantamiento de observacion)
       <Main>
         {/** Acá van los componentes de tipo Page */}
+<<<<<<< HEAD
 >>>>>>> 1006d1e (MS-5 - Create component Statistics)
+=======
+        <Statistics></Statistics>
+        <OurDepartament></OurDepartament>
+>>>>>>> efada3b (Complete MS6-ourDepartament and Update MS5-Statistic)
       </Main>
       <Footer />
     </>
