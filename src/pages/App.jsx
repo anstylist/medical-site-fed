47af8f135c386a54065a68fd58a961b377d63491
Juan Villegas <132522021+jlvillegas04@users.noreply.github.com>
2023-07-19 19:40:19 -0500

    Update App.jsx (#13)

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 import Main from "../components/Main/Main";
 import Footer from "../components/Footer/Footer";
@@ -8,15 +8,6 @@ import AppointmentForm from "../components/MS-3 Form/AppointmentForm";
 import WhoWeAre from "../components/MS-4 WhoWeAre/WhoWeAre";
 import OurDepartament from '../components/OurDepartament/OurDepartament'
 import Statistics from '../components/Statistics/Statistics'
-=======
-import React from 'react'
-import Main from '../components/Main/Main'
-import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
-import OurDepartament from '../components/OurDepartament/OurDepartament'
-import Statistics from '../components/Statistics/Statistics'
-import './App.scss'
->>>>>>> 548d70a1e1f185e7e820ae534309bd97b6722490
 import OurPartners from '../components/OurPartners/OurPartners'
 import OurTestimonials from '../components/OurTestimonials/OurTestimonials'
 
@@ -24,7 +15,6 @@ function App() {
   return (
     <>
       <Header />
-<<<<<<< HEAD
       <Main>
         <AppointmentForm />
         <WhoWeAre />
@@ -32,14 +22,6 @@ function App() {
         <OurDepartament />
         <OurTestimonials />
         <OurPartners />
-=======
-        <Statistics></Statistics>
-        <OurDepartament></OurDepartament>
-        <OurTestimonials></OurTestimonials>
-        <OurPartners></OurPartners>
-      <Main>
-        {/** Acá van los componentes de tipo Page */}
->>>>>>> 548d70a1e1f185e7e820ae534309bd97b6722490
       </Main>
       <Footer />
     </>
