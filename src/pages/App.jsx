5350523e6c0a49c2828a8bbaa20172ffdab9248e
Juan Villegas <juanvj0425@gmail.com>
2023-07-19 20:02:20 -0500

    Solving conflicts for MS3

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import React from "react";
 import Main from "../components/Main/Main";
@@ -11,10 +12,20 @@ import Statistics from '../components/Statistics/Statistics'
 import OurPartners from '../components/OurPartners/OurPartners'
 import OurTestimonials from '../components/OurTestimonials/OurTestimonials'
 
+=======
+import React from "react";
+import Main from "../components/Main/Main";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import "./App.scss";
+import AppointmentForm from "../components/MS-3 Form/AppointmentForm";
+
+>>>>>>> 4809be5 (Commit to be able to pull the carousel library from the repo)
 function App() {
   return (
     <>
       <Header />
+<<<<<<< HEAD
       <Main>
         <AppointmentForm />
         <WhoWeAre />
@@ -23,6 +34,10 @@ function App() {
         <OurTestimonials />
         <OurPartners />
       </Main>
+=======
+      <AppointmentForm />
+      <Main />
+>>>>>>> 4809be5 (Commit to be able to pull the carousel library from the repo)
       <Footer />
     </>
   );
