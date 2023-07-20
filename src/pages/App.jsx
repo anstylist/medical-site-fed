258cb6c86cfcb69d0839ef2cf04692fae1192c4c
Andry <andry.carolina88@gmail.com>
2023-07-20 00:54:42 -0500

    finish video component with modal

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,19 +1,19 @@
-
-import React from "react";
-import Main from "../components/Main/Main";
-import Footer from "../components/Footer/Footer";
-import Header from "../components/Header/Header";
-import "./App.scss";
-import AppointmentForm from "../components/MS-3 Form/AppointmentForm";
-import WhoWeAre from "../components/MS-4 WhoWeAre/WhoWeAre";
+import React from 'react'
+import Main from '../components/Main/Main'
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
+import './App.scss'
+import AppointmentForm from '../components/MS-3 Form/AppointmentForm'
+import WhoWeAre from '../components/MS-4 WhoWeAre/WhoWeAre'
 import OurDepartament from '../components/OurDepartament/OurDepartament'
 import Statistics from '../components/Statistics/Statistics'
 import OurPartners from '../components/OurPartners/OurPartners'
 import OurTestimonials from '../components/OurTestimonials/OurTestimonials'
 import GalleryCarousel from '../components/MS-8 GalleryCarousel/GalleryCarousel'
 import LatestNews from '../components/Latest News/LatestNews'
+import WideVideoSection from '../components/WideVideoSection/WideVideoSection'
 
-function App() {
+function App () {
   return (
     <>
       <Header />
@@ -22,6 +22,7 @@ function App() {
         <WhoWeAre />
         <Statistics />
         <OurDepartament />
+        <WideVideoSection />
         <GalleryCarousel />
         <OurTestimonials />
         <OurPartners />
@@ -29,7 +30,7 @@ function App() {
       </Main>
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
