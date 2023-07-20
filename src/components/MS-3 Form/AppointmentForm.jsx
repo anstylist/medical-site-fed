e2242826a33e19009aa3dc5d4fe674b280ff6584
Juan Villegas <juanvj0425@gmail.com>
2023-07-19 20:03:04 -0500

    Solving conflicts for MS3_2

--- src/components/MS-3 Form/AppointmentForm.jsx
+++ src/components/MS-3 Form/AppointmentForm.jsx
@@ -15,7 +15,6 @@ const AppointmentForm = () => {
     // Logic to handle the form submission goes here
     console.log("Form submitted!");
   };
-<<<<<<< HEAD
 
   return (
     <div className="main-banner-area">
@@ -38,55 +37,26 @@ const AppointmentForm = () => {
           <li className="banner-list__item">
             <FaHandHoldingMedical className="banner-list__icons" />
             <p className="banner-list__undertext">Primary Care</p>
-=======
-  return (
-    <div className="main-banner-area">
-      <div className="main-banner-container">
-        <span>Welcome to Mebid</span>
-        <h1>We are by your side in any services</h1>
-        <p>
-          We provide all kinds of medical services to our patients according to
-          their
-          <br></br>
-          daily needs starting from special conditions
-        </p>
-        <ul className="banner-list">
-          <li>
-            <FaAmbulance />
-            <span>Urgent Care</span>
-          </li>
-          <li>
-            <FaHandHoldingMedical />
-            <span>Primary Care</span>
->>>>>>> 4809be5 (Commit to be able to pull the carousel library from the repo)
           </li>
         </ul>
       </div>
       <div className="main-form-container">
-<<<<<<< HEAD
         <h2 className="main-form-container__h2">Book Appointment</h2>
         <p className="main-form-container__p">
           Fillup the form to make an appointment with the doctor
         </p>
-=======
-        <h2>Appointment Booking</h2>
->>>>>>> 4809be5 (Commit to be able to pull the carousel library from the repo)
         <input
           type="text"
           placeholder="Full Name"
           value={fullName}
           onChange={(e) => setFullName(e.target.value)}
-<<<<<<< HEAD
           className="main-form-container__input"
-=======
->>>>>>> 4809be5 (Commit to be able to pull the carousel library from the repo)
         />
         <input
           type="email"
           placeholder="Email Address"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-<<<<<<< HEAD
           className="main-form-container__input"
         />
         <input
@@ -110,24 +80,6 @@ const AppointmentForm = () => {
           value={selectedDoctor}
           onChange={(e) => setSelectedDoctor(e.target.value)}
           className="main-form-container__select"
-=======
-        />
-        <input
-          type="text"
-          placeholder="Booking Date"
-          value={bookingDate}
-          onChange={(e) => setBookingDate(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Department"
-          value={department}
-          onChange={(e) => setDepartment(e.target.value)}
-        />
-        <select
-          value={selectedDoctor}
-          onChange={(e) => setSelectedDoctor(e.target.value)}
->>>>>>> 4809be5 (Commit to be able to pull the carousel library from the repo)
         >
           <option value="">Select a Doctor</option>
           <option value="doctor1">Doctor 1</option>
@@ -138,7 +90,6 @@ const AppointmentForm = () => {
           placeholder="Your Message"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
-<<<<<<< HEAD
           className="main-form-container__textarea"
         ></textarea>
         <button
@@ -148,10 +99,6 @@ const AppointmentForm = () => {
         >
           Book appointment
         </button>
-=======
-        ></textarea>
-        <button onClick={handleFormSubmit}>Book Appointment</button>
->>>>>>> 4809be5 (Commit to be able to pull the carousel library from the repo)
       </div>
     </div>
   );
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import React from "react";
 import Main from "../components/Main/Main";
@@ -12,20 +11,10 @@ import Statistics from '../components/Statistics/Statistics'
 import OurPartners from '../components/OurPartners/OurPartners'
 import OurTestimonials from '../components/OurTestimonials/OurTestimonials'
 
-=======
-import React from "react";
-import Main from "../components/Main/Main";
-import Footer from "../components/Footer/Footer";
-import Header from "../components/Header/Header";
-import "./App.scss";
-import AppointmentForm from "../components/MS-3 Form/AppointmentForm";
-
->>>>>>> 4809be5 (Commit to be able to pull the carousel library from the repo)
 function App() {
   return (
     <>
       <Header />
-<<<<<<< HEAD
       <Main>
         <AppointmentForm />
         <WhoWeAre />
@@ -34,10 +23,6 @@ function App() {
         <OurTestimonials />
         <OurPartners />
       </Main>
-=======
-      <AppointmentForm />
-      <Main />
->>>>>>> 4809be5 (Commit to be able to pull the carousel library from the repo)
       <Footer />
     </>
   );
