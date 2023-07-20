00e533d7bf43d45bd0e6c5b4ea044ec6e2ea9741
Carlos <carlosmendoza99@hotmail.es>
2023-07-20 03:46:35 -0500

    ComponentFAQ

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Main from '../components/Main/Main'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
-import Faq from '../components/FAQ/Faq'
 import './App.scss'
 
 function App () {
@@ -10,7 +9,6 @@ function App () {
     <>
       <Header />
       <Main />
-      <Faq />
       <Footer />
     </>
   )
