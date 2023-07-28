06bddf426698eb3d59c3d4cf90ae8a6c11fa5608
Juan Villegas <juanvj0425@gmail.com>
2023-07-28 11:08:13 -0500

    changed main file to avoid conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.scss'
-import ComingSoon from '../src/components/ComingSoon/ComingSoon'
+import App from './pages/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ComingSoon />
+    <App />
   </React.StrictMode>
 )
