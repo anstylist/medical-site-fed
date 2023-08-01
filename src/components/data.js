237ee2b9a3aa95d6ba41418703b378b131651894
Erick_Carrasco <erickfabianct@gmail.com>
2023-08-01 13:35:35 -0500

    :pushpin: create component MS-33-find-doctor

--- src/components/data.js
+++ src/components/data.js
@@ -24,3 +24,49 @@ export const dataLink = () => {
   ]
   return link
 }
+
+export const dataCountries = () => {
+  const countries = [
+    { id: 1, name: 'Australia' },
+    { id: 2, name: 'Canada' },
+    { id: 3, name: 'Peru' },
+    { id: 4, name: 'Colombia' },
+    { id: 5, name: 'Japan' },
+    { id: 6, name: 'New zeland' },
+    { id: 7, name: 'Germany' },
+    { id: 8, name: 'Switzerland' },
+    { id: 9, name: 'United states' },
+    { id: 10, name: 'United Kingdom' },
+    { id: 11, name: 'Denmark' },
+    { id: 12, name: 'Norway' },
+    { id: 13, name: 'Estonia' },
+    { id: 14, name: 'Mexico' },
+    { id: 15, name: 'Spain' }
+  ]
+  return countries
+}
+
+export const dataHospitals = () => {
+  const hospitals = [
+    { id: 1, name: 'Augu Hospital' },
+    { id: 2, name: 'Fria Hospital' },
+    { id: 3, name: 'Kadi Hospital' },
+    { id: 4, name: 'Leve Hospital' },
+    { id: 5, name: 'Mebid Hospital' },
+    { id: 6, name: 'Qyee Hospital' },
+    { id: 7, name: 'Spix Hospital' },
+    { id: 8, name: 'Tinzer Hospital' }
+  ]
+  return hospitals
+}
+
+export const dataDoctor = () => {
+  const doctors = [
+    { id: 1, name: 'Kasey burt' },
+    { id: 2, name: 'Lyle Kauffman' },
+    { id: 3, name: 'Larry lin' },
+    { id: 4, name: 'Victoria roach' },
+    { id: 5, name: 'David collins' }
+  ]
+  return doctors
+}
