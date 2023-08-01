8d1505337f96467614c9f934e3422ce45ebdbe62
Carlos <carlosmendoza99@hotmail.es>
2023-08-01 18:10:30 -0500

    restablecer App

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 import Main from '../components/Main/Main'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
-
+import Home from '../components/Home/Home'
 import './App.scss'
-import About from './About/About'
 
 function App () {
   return (
     <>
       <Header />
       <Main>
-        <About/>
+        <Home />
       </Main>
       <Footer />
     </>
