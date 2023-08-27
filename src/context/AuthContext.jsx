4f4b87c90334cfbf40e23620816ced65a421ee33
Andry <andry.carolina88@gmail.com>
2023-08-26 21:01:42 -0500

    add function to update the global status of my users persistently, MS-46

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -11,9 +11,25 @@ export const AuthProvider = ({ children }) => {
     role: localStorage.getItem('role') || ''
   })
 
+  const updateAuthData = ({ fullName = '', email = '', status = '', token = '', role = '' } = {}) => {
+    setAuthData({
+      fullName,
+      email,
+      status,
+      token,
+      role
+    })
+
+    localStorage.setItem('fullName', fullName)
+    localStorage.setItem('email', email)
+    localStorage.setItem('status', status)
+    localStorage.setItem('token', token)
+    localStorage.setItem('role', role)
+  }
+
   return (
-    < AuthContext.Provider
-      value={{ authData, setAuthData }}
+    <AuthContext.Provider
+      value={{ authData, updateAuthData }}
     >
       {children}
     </AuthContext.Provider>
